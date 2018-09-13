8fb014367bb61eba9558dbf45b881046cc4ad70f
PurnaChandraPanda <pupanda@outlook.com>
2018-09-13 07:12:47 +0530

    first commit

---
+++ PromptDialogTestingBot/Controllers/MessagesController.cs
@@ -0,0 +1,68 @@
+﻿using Autofac;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
+using System.Linq;
+
+namespace PromptDialogTestingBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.GetActivityType() == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            string messageType = message.GetActivityType();
+            if (messageType == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (messageType == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+
+                //MicrosoftAppCredentials.AutoTokenRefreshTimeSpan = TimeSpan.FromSeconds(30);
+                //ConnectorClient client = new ConnectorClient(new Uri(activity.ServiceUrl), new MicrosoftAppCredentials());
+                //await client.Conversations.ReplyToActivityAsync(activity.CreateReply(activity.Text));
+            }
+            else if (messageType == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (messageType == ActivityTypes.Typing)
+            {
+                // Handle knowing that the user is typing
+            }
+            else if (messageType == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ PromptDialogTestingBot/Dialogs/RootDialog.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using static Microsoft.Bot.Builder.Dialogs.PromptDialog;
+
+namespace PromptDialogTestingBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        protected int count = 1;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            if (activity.Text.Equals("reset", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Show "reset" question with yes/ no option, and read yes/ no only
+                //ResetOptionSimple(context);
+
+                // Show "reset" question with yes/ no option, and allow pattern to read
+                ResetOptionWithYesNoPattern(context);
+
+                // Show "reset" question without yes/ no option, and allow pattern to read
+                //ResetOptionWithoutYesNoPattern(context);
+
+                // Show "reset" option with choice
+                //ResetOptionWithChoice(context);
+            }
+            else
+            {
+                // Calculate something for us to return
+                int length = (activity.Text ?? string.Empty).Length;
+
+                // Return our reply to the user
+                await context.PostAsync($"{count++}. You sent {activity.Text} which was {length} characters");
+
+                context.Wait(MessageReceivedAsync);
+            }                        
+        }
+
+        private async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> result)
+        {
+            var confirm = await result;
+            if (confirm)
+            {
+                this.count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private void ResetOptionSimple(IDialogContext context)
+        {
+            PromptDialog.Confirm(context, AfterResetAsync, "Are you sure you want to reset?", promptStyle: PromptStyle.Auto);
+        }
+
+        private void ResetOptionWithYesNoPattern(IDialogContext context)
+        {
+            var pattern = new string[][] {
+                    new string[]{ "yes", "yeah", "yep", "yeppy" },
+                    new string[]{ "no", "nope", "nono", "nop" }
+                };
+
+            var promptOption = new PromptOptions<string>("Are you sure you want to reset?", null, null,
+                                                                options: PromptConfirm.Options, promptStyler: new PromptStyler(promptStyle: PromptStyle.Auto));
+            PromptDialog.Confirm(context, AfterResetAsync, promptOption, pattern);
+        }
+
+        private void ResetOptionWithoutYesNoPattern(IDialogContext context)
+        {
+            var pattern = new string[][] {
+                    new string[]{ "yes", "yeah", "yep", "yeppy" },
+                    new string[]{ "no", "nope", "nono", "nop" }
+                };
+
+            var promptOption = new PromptOptions<string>("Are you sure you want to reset?");
+            PromptDialog.Confirm(context, AfterResetAsync, promptOption, pattern);
+        }
+
+        private void ResetOptionWithChoice(IDialogContext context)
+        {
+            PromptDialog.Choice<ResetOptions>(
+                context: context,                
+                resume: AfterChoiceSelected,
+                options: (IEnumerable<ResetOptions>)Enum.GetValues(typeof(ResetOptions)),
+                prompt: "Are you sure you want to reset?",
+                retry: "Please try again.",
+                promptStyle: PromptStyle.Auto
+                );
+        }
+
+        private async Task AfterChoiceSelected(IDialogContext context, IAwaitable<ResetOptions> result)
+        {
+            var confirm = await result;
+            if (confirm == ResetOptions.Yes)
+            {
+                this.count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private enum ResetOptions
+        {
+            Yes,
+            No,
+            Thankyou
+        }
+    }
+}
